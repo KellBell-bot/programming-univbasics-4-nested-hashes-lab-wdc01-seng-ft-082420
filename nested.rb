e5367aeaa676b18b14e4dd86e22e636eeb6dac80
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-13 15:26:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,7 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
+  
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -75,8 +76,8 @@ def adding_matz
       }
     }
 programmer_hash[:yukihiro_matsumoto]= {
-  :known_for => "Ruby",
-  :languages => ["LISP", "C"]
+  :known_for=> "Ruby",
+  :languages=> ["LISP", "C"]
   }
 end
 
